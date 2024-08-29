35a9cd83158894da851f25312a83aa4fc1537f79
mykolatsiapalo1 <mykolatsiapalo1@gmail.com>
2024-08-29 16:37:16 +0300

    add Modal

---
+++ src/app/components/Modal/Modal.tsx
@@ -0,0 +1,42 @@
+import { forwardRef, useImperativeHandle, useRef, ReactNode } from "react";
+
+interface ModalProps {
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal = forwardRef<{ openModal: () => void }, ModalProps>(
+  ({ onClose, children }, ref) => {
+    const modalRef = useRef<HTMLDialogElement>(null);
+
+    const openModal = () => {
+      modalRef.current?.showModal();
+    };
+
+    const closeModal = () => {
+      modalRef.current?.close();
+      onClose();
+    };
+
+    useImperativeHandle(ref, () => ({
+      openModal,
+    }));
+
+    return (
+      <dialog ref={modalRef} className="modal">
+        <div className="modal-box w-11/12 max-w-5xl">
+          {children}
+          <div className="modal-action">
+            <button className="btn" onClick={closeModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      </dialog>
+    );
+  }
+);
+
+Modal.displayName = "Modal";
+
+export default Modal;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@
 
 import Button from "./components/Button/Button";
 import ButtonEdit from "./components/Button/ButtonEdit";
+import Modal from "./components/Modal/Modal";
+import { useRef } from "react";
 
 export default function Home() {
+  const modalRef = useRef<{ openModal: () => void }>(null);
+
   const handleClick = () => {
     console.log("Click");
+  };
+  const handleModalClose = () => {
+    console.log("Modal closed");
+  };
+  const handleOpenModal = () => {
+    if (modalRef.current) {
+      modalRef.current.openModal();
+    }
   };
   return (
     <main>
@@ -18,7 +30,18 @@ export default function Home() {
       <Button onClick={handleClick} size="large">
         Large Button
       </Button>
+      
       <ButtonEdit onClick={handleClick} />
+
+      <Button onClick={handleOpenModal} size="small">
+        Open
+      </Button>
+
+      <Modal ref={modalRef} onClose={handleModalClose}>
+        <h3 className="font-bold text-lg">Hello!</h3>
+        <p className="py-4">This is a reusable modal with custom content.</p>
+        <button className="btn">Another button inside modal</button>
+      </Modal>
     </main>
   );
 }
